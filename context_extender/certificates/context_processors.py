3162b8c6e5749217f55d4a9eeb424d7a9bc839ff
Thomas Peet <tomaspemora@gmail.com>
2024-08-26 15:06:17 -0400

    fix: extract user_id from GeneratedCertificate using verification_uuid and improve program grades context handling.

--- context_extender/certificates/context_processors.py
+++ context_extender/certificates/context_processors.py
@@ -20,8 +20,6 @@ def add_student_program_grade(request):
     # Get the current view name
     current_view_name = resolve(request.path_info).url_name
     if current_view_name in ['render_cert_by_uuid','preview_cert']:
-        user_id = request.user.id
-        course_id = None
         # Extract the verification UUID from the request (if available)    
         verification_uuid = request.resolver_match.kwargs.get('certificate_uuid')
         if verification_uuid:
@@ -29,36 +27,35 @@ def add_student_program_grade(request):
                 # Verified Certificate
                 certificate = GeneratedCertificate.objects.get(verify_uuid=verification_uuid)
                 course_id = certificate.course_id
-                logger.info(f"Extracted course_id from certificate with UUID {verification_uuid}: {course_id}")
+                user_id = certificate.user_id
+                logger.info(f"Extracted course_id and user_id from certificate with UUID {verification_uuid}: course_id - {course_id}, user_id - {user_id}")
             except GeneratedCertificate.DoesNotExist:
                 logger.warning(f"No certificate found with UUID {verification_uuid}")
         else:
             # Extract course_id from resolver_match kwargs as a fallback
-            course_id = request.resolver_match.kwargs.get('course_id')
-            logger.info(f"Extracted course_id from resolver_match: {course_id}")
+            logger.warning(f"No certificate found with UUID {verification_uuid}")
 
         # Fetch the student's grade if we have a course_id
-        if course_id:
+        if course_id and user_id:
             program = EolCourseProgram.objects.filter(final_course_id=course_id).first()
             if program:  # Check if program is not None
                 program_courses = program.courses.all()
                 if program_courses:  # Check if program_courses is not empty
-                    grades = []
-                    scaled_grades = []
+                    grades = {}
+                    scaled_grades = {}
                     for course in program_courses:
                         course_grade = PersistentCourseGrade.objects.filter(user_id=user_id, course_id=course.id).first()
-                        grades.append(round(course_grade.percent_grade * 100,3))
-                        
+                        grades[str(course.id)] = round(course_grade.percent_grade * 100,3)
                         course_key = CourseKey.from_string(str(course.id))
                         course_info = get_course_with_access(request.user, "load", course_key)
-                        grade_cutoff = min(course_info.grade_cutoffs.values())*100
-                        scaled_grades.append(round(calculate_grade(course_grade.percent_grade * 100, approval_percent=grade_cutoff),3))
+                        grade_cutoff = min(course_info.grade_cutoffs.values()) * 100
+                        scaled_grades[str(course.id)] = round(calculate_grade(course_grade.percent_grade * 100, approval_percent=grade_cutoff),3)
                     logger.info(grades)
                     if grades:  # Check if grades list is not empty
                         context['student_program_grades'] = grades
                         context['student_program_scaled_grades'] = scaled_grades
-                        context['student_program_percent_grade_avg'] = round(mean(grades),3)
-                        context['student_program_scaled_grade_avg'] = round(mean(scaled_grades),3)
+                        context['student_program_percent_grade_avg'] = round(mean(list(grades.values())),3)
+                        context['student_program_scaled_grade_avg'] = round(mean(list(scaled_grades.values())),3)
                     else:
                         logger.info('No grades available for the program courses.')
                 else:
@@ -66,7 +63,7 @@ def add_student_program_grade(request):
             else:
                 logger.info('No program found for the given course_id.')
         else:
-            logger.info('No course_id provided.')
+            logger.info('No course_id and/or user_id found within the certificate.')
     
     return context
 
@@ -76,8 +73,6 @@ def add_student_grade(request):
     # Get the current view name
     current_view_name = resolve(request.path_info).url_name
     if current_view_name in ['render_cert_by_uuid','preview_cert']:
-        user_id = request.user.id
-        course_id = None
         # Get the current view name
         current_view_name = resolve(request.path_info).url_name
         # Extract the verification UUID from the request (if available)
@@ -87,13 +82,13 @@ def add_student_grade(request):
                 # Verified Certificate
                 certificate = GeneratedCertificate.objects.get(verify_uuid=verification_uuid)
                 course_id = certificate.course_id
+                user_id = certificate.user_id
                 logger.info(f"Extracted course_id from certificate with UUID {verification_uuid}: {course_id}")
             except GeneratedCertificate.DoesNotExist:
                 logger.warning(f"No certificate found with UUID {verification_uuid}")
         else:
             # Extract course_id from resolver_match kwargs as a fallback
-            course_id = request.resolver_match.kwargs.get('course_id')
-            logger.info(f"Extracted course_id from resolver_match: {course_id}")
+            logger.warning(f"No certificate found with UUID {verification_uuid}")
 
         # Fetch the student's grade if we have a course_id
         if course_id:
@@ -111,7 +106,18 @@ def add_student_document(request):
     # Get the current view name
     current_view_name = resolve(request.path_info).url_name
     if current_view_name in ['render_cert_by_uuid','preview_cert']:
-        user_id = request.user.id
+        verification_uuid = request.resolver_match.kwargs.get('certificate_uuid')
+        if verification_uuid:
+            try:
+                # Verified Certificate
+                certificate = GeneratedCertificate.objects.get(verify_uuid=verification_uuid)
+                user_id = certificate.user_id
+                logger.info(f"Extracted user_id from certificate with UUID {verification_uuid}: user_id - {user_id}")
+            except GeneratedCertificate.DoesNotExist:
+                logger.warning(f"No certificate found with UUID {verification_uuid}")
+        else:
+            # Extract course_id from resolver_match kwargs as a fallback
+            logger.warning(f"No certificate found with UUID {verification_uuid}")
         # Get the current view name
         current_view_name = resolve(request.path_info).url_name        
         # Fetch additional data from eol_sso_login_ssologinextradata
